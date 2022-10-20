8740f649ec0b7c9332be86fd901f418fe7111df5
Javier Tintaya <jhosimart42@gmail.com>
2022-10-20 18:36:07 -0400

    Cambios en el archivo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
     colors: {
       primary: "#ffff",
       secondary: "#2e2e",
+      amber1: {
+        100: "#cffafe",
+        200: "#a5f3fc",
+        300: "#67e8f9",
+        400: "#22d3ee",
+        500: "#06b6d4",
+        600: "#0891b2",
+        700: "#0e7494",
+        800: "#155e75",
+        900: "#164e63",
+      },
+    },
+    extend: {
+      colors: {
+        blue: {
+          950: "#17275c",
+        },
+      },
     },
   },
   plugins: [],
