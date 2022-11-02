c1789e9763e2564dcc8d0a62ee522b6b06a8f9d7
Javier Tintaya <jhosimart42@gmail.com>
2022-11-01 20:11:28 -0400

    Archivo de configuracion modificado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,8 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    colors: {
-      primary: "#ffff",
-      secondary: "#2e2e",
-      amber1: {
-        100: "#cffafe",
-        200: "#a5f3fc",
-        300: "#67e8f9",
-        400: "#22d3ee",
-        500: "#06b6d4",
-        600: "#0891b2",
-        700: "#0e7494",
-        800: "#155e75",
-        900: "#164e63",
-      },
-    },
     extend: {
-      colors: {
-        blue: {
-          950: "#17275c",
-        },
-      },
+      colors: {},
       backgroundImage: {
         sf: "url('https://images.pexels.com/photos/1141853/pexels-photo-1141853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
         sanFrancisco: "./img/sanFrancisco.jpg",
@@ -30,6 +11,17 @@ module.exports = {
           "url('https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
         chicago:
           "url('https://images.pexels.com/photos/1569012/pexels-photo-1569012.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#cc2d4a",
+        secondary: "#8fa206",
+        terciary: "#61aec9",
+      }),
+      textColor: {
+        primary: "#cc2d4a",
+        secondary: "#8fa206",
+        terciary: "#61aec9",
       },
     },
   },
